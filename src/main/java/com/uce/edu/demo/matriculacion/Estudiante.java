cc4b158292d2170afea406a674f63f0154462646
Michael Cen <cenyungamichael@gmail.com>
2022-05-28 19:19:43 -0500

    Tarea 4

---
+++ src/main/java/com/uce/edu/demo/matriculacion/Estudiante.java
@@ -0,0 +1,33 @@
+package com.uce.edu.demo.matriculacion;
+
+public class Estudiante {
+	private String nombre;
+	private String apellido;
+	private String fecha_nacimiento;
+
+	// Get y Set
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(String fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+}
---
+++ src/main/java/com/uce/edu/demo/matriculacion/Matricula.java
@@ -0,0 +1,26 @@
+package com.uce.edu.demo.matriculacion;
+
+public class Matricula {
+
+	private Estudiante estudiante;
+	private Materia materia;
+
+	// Get y Set
+
+	public Estudiante getEstudiante() {
+		return estudiante;
+	}
+
+	public void setEstudiante(Estudiante estudiante) {
+		this.estudiante = estudiante;
+	}
+
+	public Materia getMateria() {
+		return materia;
+	}
+
+	public void setMateria(Materia materia) {
+		this.materia = materia;
+	}
+
+}
